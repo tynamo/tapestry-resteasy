2f5cf4669e4aac5bb4de05647543bccbb88639e0
Alejandro Scandroli <ascandroli@codehaus.org>
2011-11-14 21:47:03 +0000

    * removing the tesng.xml file * upgrading to T5.3 (careful! now the JAX-RS annotations must be on the interfaces or RESTEasy will not be able to find them)

--- src/test/java/org/tynamo/resteasy/ws/ReloadableEchoResource.java
+++ src/test/java/org/tynamo/resteasy/ws/ReloadableEchoResource.java
@@ -1,9 +1,14 @@
 package org.tynamo.resteasy.ws;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
+@Path("/echo")
 public interface ReloadableEchoResource
 {
+	@GET
+	@Path("/{message}")
 	Response echo(@PathParam("message") String message);
 }
--- src/test/java/org/tynamo/resteasy/ws/ReloadableEchoResourceImpl.java
+++ src/test/java/org/tynamo/resteasy/ws/ReloadableEchoResourceImpl.java
@@ -1,20 +1,14 @@
 package org.tynamo.resteasy.ws;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
-@Path("/echo")
 public class ReloadableEchoResourceImpl implements ReloadableEchoResource
 {
 
-	@GET
-	@Path("/{message}")
 	@Override
-public Response echo(@PathParam("message") String message)
-{
-	return Response.status(200).entity(message).build();
-}
+	public Response echo(String message)
+	{
+		return Response.status(200).entity(message).build();
+	}
 
 }
