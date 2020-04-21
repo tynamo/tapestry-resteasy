1c5a143fa31d7c1633eee5421987ad46c2df4c3f
kaosko <kaosko@apache.org>
2020-04-21 20:46:36 +0300

    Remove swagger dependencies
    - some swagger annotation were still used in test classes

--- src/test/java/org/tynamo/resteasy/ws/ReloadableEchoResource.java
+++ src/test/java/org/tynamo/resteasy/ws/ReloadableEchoResource.java
@@ -6,16 +6,11 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
-@Api(value = "/echo", description = "the ECHO api")
 @Path("/echo")
 public interface ReloadableEchoResource
 {
 	@GET
 	@Path("/{message}")
 	@Produces("application/json")
-	@ApiOperation("echoes a message")
 	Response echo(@PathParam("message") String message);
 }
